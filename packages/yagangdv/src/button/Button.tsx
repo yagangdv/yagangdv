72192529651890fdc2b9c0d22657e9e1d8199188
chenyagang <chenyagang333@outlook.com>
2026-07-19 19:21:29 +0800

    feat: a

--- packages/yagangdv/src/button/Button.tsx
+++ packages/yagangdv/src/button/Button.tsx
@@ -29,7 +29,7 @@ const Button = defineComponent<
 >(
   (_, { slots }) => {
     return () => {
-      return <div>{slots.default?.()}</div>
+      return <div class="yagang-button-333">{slots.default?.()}</div>
     }
   },
   {
